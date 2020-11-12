08d5d51c3fc319eb370d120eb84691d97220c7bd
smokey18 <kmboplays@gmail.com>
2020-11-12 14:26:10 +0100

    Havoc time
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_mido.mk
+    $(LOCAL_DIR)/havoc_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_mido-eng \
-    ancient_mido-userdebug \
-    ancient_mido-user
+    havoc_mido-eng \
+    havoc_mido-userdebug \
+    havoc_mido-user
